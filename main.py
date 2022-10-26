e0b3fa01e9c9ab30ea9c5b7268d8d1fc1d449168
Kehinde Ajasa <ti664469@gmail.com>
2022-10-26 21:31:15 +0100

    initial files created

---
+++ main.py
@@ -0,0 +1,7 @@
+"""This code is used to solve the word cookies game"""
+
+
+def open_file(filename):
+    """Function to help open the file where the existing english language wo
+    rds are present"""
+    pass
