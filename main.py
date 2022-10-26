ddac6bebcd0b308a1521e6e38db3d3b87246b9cf
Kehinde Ajasa <ti664469@gmail.com>
2022-10-26 23:25:34 +0100

    function to help sort out the result gotten into the amount of letter words it contains

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import itertools
 
 
 def get_user_input():
-    """function to help accept all the user's inputs"""
-    word_cookie = 'currency'  # input('Enter the word in the word cookie tray(in any order): ')
+    """Function to help accept the user's input and store"""
+    word_cookie = input('Enter the word in the word cookie tray(in any order):  ')
     return word_cookie
 
 
 def open_dict_file():
     """function to help open up the dictionary file for accessing all the english language words"""
-    with open('C:\\Users\\MY PC\\Desktop\\PYTHON PROJECTS\\RHEMA VERSIONS\\my.txt', 'r') as file:  # opening the file
+    with open('C:\\Users\\MY PC\\Desktop\\PYTHON PROJECTS\\Dictionary.txt', 'r') as file:  # opening the file
         dictionary = file.read()
         # using the .split method to convert each word in the file to func individual and collectively as func list
         words = dictionary.split()
@@ -45,4 +45,23 @@ def create_anagrams(function1, function2):
     return set(final_result)
 
 
-print((create_anagrams(sort_dictionary(open_dict_file()), get_user_input())))
+def n_letter_words(data):
+    """Function to arrange the result gotten in terms of length of letters"""
+    arranged_results = dict()
+    for word in data:
+        length_of_word = len(word)
+        if str(length_of_word) in arranged_results:
+            arranged_results[str(length_of_word)] += f', {word} '
+        else:
+            arranged_results[str(length_of_word)] = word
+    return arranged_results
+
+
+def display_final_result(data):
+    """Function to return the final result in a formatted loop"""
+    for result in data:
+        print(f"{result} letter words-->{data[result]}")
+
+
+display_final_result(n_letter_words((create_anagrams(sort_dictionary(open_dict_file()), get_user_input()))))
+
