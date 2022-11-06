0e9defcf8e7e0d5679295cc3778e81352d6519cd
unknown <sojapril16@gmail.com>
2022-11-06 23:37:47 +0100

    drop down

--- cookiesolver/static/cookiesolver/solution.js
+++ cookiesolver/static/cookiesolver/solution.js
@@ -15,7 +15,13 @@ document.addEventListener('DOMContentLoaded',function(){
             btn.style.opacity = '1'
         }
     }
-    console
+
+    let word_count = 'All'
+
+    document.querySelector('select').onchange = function(select){
+        word_count = this.value
+    }
+
     // function gets user input save it into a variable and send it to django view to work with through the sendUserInput function
     document.querySelector('button').addEventListener('click',function(){
         const user_input = document.querySelector('input').value
@@ -23,20 +29,29 @@ document.addEventListener('DOMContentLoaded',function(){
             alert('Input must not be grater than 6 words')
         }
         else{
-            // 
-            document.querySelector('.shadow').style.display = 'block'
-            // 
-            var result_container = document.querySelector('.letters')
-            result_container.innerHTML = " "
-            // 
-            const input_value = document.querySelector('input')
-            // 
-            sendUserInput(input_value.value.toLowerCase())
-            // 
-            retrieveOutput()
-            // 
-            document.querySelector('input').value = ""
-            setTimeout(popup,5000)
+            if (word_count > user_input.length){
+                alert('Letter count must be equal to or greater than entered value')
+            }
+            else{
+                // 
+                document.querySelector('.shadow').style.display = 'block'
+                // 
+                var result_container = document.querySelector('.letters')
+                result_container.innerHTML = " "
+                // 
+                const input_value = document.querySelector('input')
+                // 
+                sendUserInput(input_value.value.toLowerCase())
+                // 
+                if (word_count == "All"){
+                    retrieveOutput('All')
+                }
+                else{
+                    retrieveOutput(parseInt(word_count))
+                }
+                // 
+                setTimeout(popup,5000)
+            }
         }
     })
 
@@ -47,7 +62,6 @@ document.addEventListener('DOMContentLoaded',function(){
         //  
     }
     
-    /////////////////
     // function send user input to django views
     function sendUserInput(vaue){
         fetch('/getUserInput',{
@@ -57,27 +71,62 @@ document.addEventListener('DOMContentLoaded',function(){
             })
         })
     }
-    //////////////////
+
     // function retrieves possible word that can be formed by the letter entered by the user and appends them to the webpage
-    function retrieveOutput(){
+    function retrieveOutput(numb){
         fetch('/retrieve')
         .then(response => response.json())
         .then(data => {
-            const values = data.answer
-            // looping through dict
-            for (const words in values){
-                if(values[words].length > 0 ){
-                    //
+            const result_length = Object.keys(data.answer).length
+            if (result_length > 0){
+                // console.log(data.answer)
+                if (numb == 'All'){
+                    const values = data.answer
+                    // looping through dict
+                    for (const words in values){
+                        if(values[words].length > 0 ){
+                            //
+                            const div_element = document.createElement('div')
+                            div_element.style.cssText = `margin-bottom:2em`
+                            div_element.className = 'n_words_container'
+                            // 
+                            const h1_element = document.createElement('h1')
+                            h1_element.innerHTML = `${words} Letters`
+                            h1_element.className = 'count_words'
+                            div_element.append(h1_element)
+                            // 
+                            values[words].forEach(function(word){
+                                const letters_section = document.createElement('section')
+                                letters_section.style.cssText = `display:flex;flex-direction:row;margin-bottom:2em`
+                                letters_section.className = 'word_block'
+                                // convert individual elements in an array into an array
+                                list_word = word.split('')
+                                list_word.forEach(function(list_word){
+                                    const letter_container = document.createElement('div')
+                                    letter_container.className = 'letter_block'
+                                    letter_container.innerHTML = list_word
+                                    letters_section.append(letter_container)
+                                })
+                                div_element.append(letters_section)
+                            })
+                            document.querySelector('.letters').append(div_element)
+                            document.querySelector('.letters').style.opacity = 0
+                        }
+                    }
+                }
+                else{
+                    const values = data.answer
+                    // 
                     const div_element = document.createElement('div')
                     div_element.style.cssText = `margin-bottom:2em`
                     div_element.className = 'n_words_container'
                     // 
                     const h1_element = document.createElement('h1')
-                    h1_element.innerHTML = `${words} Letters`
+                    h1_element.innerHTML = `${numb} Letters`
                     h1_element.className = 'count_words'
                     div_element.append(h1_element)
                     // 
-                    values[words].forEach(function(word){
+                    values[numb].forEach(function(word){
                         const letters_section = document.createElement('section')
                         letters_section.style.cssText = `display:flex;flex-direction:row;margin-bottom:2em`
                         letters_section.className = 'word_block'
@@ -94,6 +143,9 @@ document.addEventListener('DOMContentLoaded',function(){
                     document.querySelector('.letters').append(div_element)
                     document.querySelector('.letters').style.opacity = 0
                 }
+            }
+            else{
+                alert('Enter a valid word')
             }
         })
     }
